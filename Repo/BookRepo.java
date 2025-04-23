e2aa4e19f4b1937ff33c4121853709dccb7fb155
garveshtiwari <garveshtiwari1998@gmail.com>
2025-04-23 15:04:56 +0530

    Add files via upload

---
+++ Repo/BookRepo.java
@@ -0,0 +1,34 @@
+package extra.Library_Management_System.Repo;
+
+import java.util.*;
+import extra.Library_Management_System.Entity.*;
+
+public class BookRepo {
+    public static List<String> bookRecords = Arrays.asList(
+        "Book1 : 9997 : Author1 : 2020 : true",
+        "Book2 : 9998 : Author2 : 2021 : false",
+        "Book3 : 10000 : TSS : 2021 : true",
+        "The Alchemist : 10001 : Jitendar : 2007 : true",
+        "Harry Potter : 10002 : Mukesh : 2002 : true",
+        "Beauty and the Beast : 10003 : Tarun : 2005 : true",
+        "Titanic : 10004 : Manas : 2008 : true",
+        "Games of Thrones : 10005 : Tisha : 2010 : true",
+        "House of Dragons : 10006 : Vidushi : 2023 : true",
+        "The Chronicles of Narnia : 10007 : Nazeer : 2025 : true",
+        "How to train your Dragon : 10008 : Ayush : 2005 : true",
+        "Conjuoring : 10009 : Tushar : 2024 : true",
+        "Wall - E : 10010 : Govardhan : 2015 : true",
+        "The Devil : 10011 : Dixhit : 2012 : true",
+        "Beauty without the Brain : 10012 : Devashruti : 2018 : true",
+        "Stranger Things : 10013 : Animesh : 2023 : true",
+        "AntGirl : 10014 : Aarya : 2014 : true",
+        "The Hulk : 10015 : Keshav : 2013 : true",
+        "Captain America Returns : 10016 : Sanjay : 2021 : true",
+        "Thor : 10017 : Ayush : 2013 : true",
+        "IronMan : 10018 : Garvesh : 2025 : true",
+        "The Grave of the Fireflies : 10019 : Rachit : 2019 : true",
+        "The Giants : 10020 : Aaditya : 2020 : true",
+        "Java 24 : 10021 : Tarun : 2024 : true"
+    );
+}
+
---
+++ Repo/UserRepo.java
@@ -0,0 +1,22 @@
+package extra.Library_Management_System.Repo;
+
+import java.util.*;
+import extra.Library_Management_System.Entity.*;
+
+public class UserRepo {
+    public static List<String> userRecords = Arrays.asList(
+        "Alice : U0001",
+        "Bob : U0002",
+        "Tarun : U0003",
+        "Tina : U1001",
+        "Raj : U1002",
+        "Sahil : U1003",
+        "Rohan : U1004",
+        "Rohit : U1005",
+        "Tanya : U1006",
+        "Varun : U1007",
+        "Ayush : U1008",
+        "Arjun : U1009",
+        "Isha : U1010"
+    );
+}
