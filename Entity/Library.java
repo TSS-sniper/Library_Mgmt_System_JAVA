f53308ed6e1d715cf686c1e8b0ce5a5a11496177
Tarun Sheoran <121627136+TSS-sniper@users.noreply.github.com>
2025-04-23 15:03:09 +0530

    Delete Entity directory

--- Entity/Library.java
+++
@@ -1,7 +0,0 @@
-package src.Entity;
-
-public interface Library {
-    void addBookToDatabase(Book book);
-    void removeBookFromDatabase(String isbn);
-    void BookRecord(); // Prints or returns all book records
-}
